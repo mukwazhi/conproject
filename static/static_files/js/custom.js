11265d38fe5875fabe4e9a3be0982d9978024c40
bmukwazhi <mukwazhi@gmail.com>
2016-05-10 10:56:44 +0200

    google map config

--- static/static_files/js/custom.js
+++ static/static_files/js/custom.js
@@ -1,17 +1,22 @@
+var myCenter=new google.maps.LatLng(-17.824858, 31.053028);
+var marker;
+
 function initialize()
 {
-  var mapProp = {
-    center: new google.maps.LatLng(-17.824858, 31.053028),
-    zoom:7,
-    panControl:true,
-    zoomControl:true,
-    mapTypeControl:true,
-    scaleControl:true,
-    streetViewControl:true,
-    overviewMapControl:true,
-    rotateControl:true,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+var mapProp = {
+  center:myCenter,
+  zoom:15,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  animation:google.maps.Animation.BOUNCE
+  });
+
+marker.setMap(map);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
